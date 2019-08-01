cb35fa6aabf6695d9ce4ebc1d396d04a6fff0a6c
YTRodi <52511564+YTRodi@users.noreply.github.com>
2019-07-31 22:02:23 -0300

    hecho el 31/7/19

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/nueve.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/nueve.js
@@ -1,4 +1,34 @@
 function mostrar()
 {
+    var marca; //entre 1 y 100 (validar (isNaN))
+    //var peso;
+    var temperatura;
+    var numero; // seria el peso
+
+    marca = prompt("Ingrese marca"); //string
+    numero = prompt("ingrese un numero");
+    marca = "pepito";
+    
+    while(isNaN(numero) || numero < 1 || numero >100)
+    {
+        numero = prompt("Error. ingrese un numero entre 1 y 100:");
+        numero = parseInt(numero);
+    }
+    
+
+
+
+
+
+
+
+
+    //document.write("La cantidad de temperatura pares es: " + +"<br>");
+
 
 }
+//a.cantidad de pares (uso la funcion modulo |x|)
+//b.cantidad de pares (uso la funcion modulo |x|)
+//c.cantidad de pares (uso la funcion modulo |x|)
+//d.cantidad de pares (uso la funcion modulo |x|)
+//e.cantidad de pares (uso la funcion modulo |x|)
