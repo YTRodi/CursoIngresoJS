0f87fbf81a6da6ffef5bd270053dd37cc4042eb0
YTRodi <52511564+YTRodi@users.noreply.github.com>
2019-08-02 16:01:29 -0300

    AGREGADO 02/08/2019

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/nueve.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/nueve.js
@@ -1,34 +1,103 @@
 function mostrar()
 {
-    var marca; //entre 1 y 100 (validar (isNaN))
-    //var peso;
-    var temperatura;
-    var numero; // seria el peso
+    // ingresar la marca del producto
+    // el peso el cual debe ser entre 1 y 100 (validar)
+    //la temperatura de almacenamiento(entre -30 y 30) hasta que el
+    //usuario quiera e informar al terminar el ingreso por document.write
 
-    marca = prompt("Ingrese marca"); //string
-    numero = prompt("ingrese un numero");
-    marca = "pepito";
-    
-    while(isNaN(numero) || numero < 1 || numero >100)
+    var marca;
+    var peso;
+    var numero;
+    var cantidad;
+    var pesoFinal;
+    var respuesta = "si";
+
+    marca = prompt("Ingrese marca del producto");
+    numero = prompt("Ingrese un numero para el peso")
+    //peso = prompt("Ingrese el peso del producto");
+    //peso = parseInt(peso);
+    numero = parseInt(numero);
+
+    //cantidad = 3;
+    //producto = "galletitas";
+    //marca = "pepito"; //|| "jorgito";
+    //marca = parseInt(marca);
+
+    while(marca == "pepito")
     {
-        numero = prompt("Error. ingrese un numero entre 1 y 100:");
-        numero = parseInt(numero);
+        while(isNaN(numero))
+        {
+            numero = prompt("error, ingresa un numero entre 1 y 100");
+            numero = parseInt(numero);
+        }
+
+        if(numero < 1)
+        {
+            numero = prompt("error, ingresa un numero entre 1 y 100");
+            numero = parseInt(numero);
+        }
+
     }
+
     
-
-
-
-
-
-
-
-
-    //document.write("La cantidad de temperatura pares es: " + +"<br>");
-
-
+    document.write("b) la marca del producto más pesado");
 }
-//a.cantidad de pares (uso la funcion modulo |x|)
-//b.cantidad de pares (uso la funcion modulo |x|)
-//c.cantidad de pares (uso la funcion modulo |x|)
-//d.cantidad de pares (uso la funcion modulo |x|)
-//e.cantidad de pares (uso la funcion modulo |x|)
+
+
+
+
+
+/*while(respuesta != "no")
+    {
+        numero = prompt("Ingrese un numero (SERIA EL PESO)");
+        numero = parseInt(numero);
+
+        while(isNaN(numero))
+        {
+            numero = prompt("Ingrese un numero, no una letra (SERIA EL PESO)");
+            numero = parseInt(numero);
+        }
+
+        if (numero>1)
+        {
+
+        }
+    }*/
+
+
+    /*switch(marca)
+    {
+        case "pepito":
+            if(numero <1)
+            {
+                numero = prompt("Error, ingrese un numero entre 1 y 100");
+                break;
+            }
+            else if(numero >100)
+            {
+                numero = prompt("Error, ingrese un numero entre 1 y 100");
+                break;
+            }
+            else
+            {
+                alert("Usted ha ingresado un peso correcto");
+            }
+        
+        case "Jorgito":
+            if(numero <1)
+            {
+                numero = prompt("Error, ingrese un numero entre 1 y 100");
+                break;
+            }
+            else if(numero >100)
+            {
+                numero = prompt("Error, ingrese un numero entre 1 y 100");
+                break;
+            }
+            else
+            {
+                alert("Usted ha ingresado un peso correcto");
+            }
+
+    }*/
+
